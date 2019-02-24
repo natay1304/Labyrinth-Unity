646dfc8ba5a1a7b2dd8fd8706863af1c02167bd7
Dmitry <proctvvv@gmail.com>
2019-02-24 20:42:56 +0200

    Создан шаблон ловушки

---
+++ Labyrinth-Unity/Assets/Scripts/TrapBlastZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TrapBlastZone : MonoBehaviour
+{
+    public UnityEvent TriggerEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEvent.Invoke();
+    }
+}

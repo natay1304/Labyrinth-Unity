a604a260935a0bd48f99929bbbf7105884a903d8
Anastasiya <natay1304@gmail.com>
2019-04-16 22:57:02 +0300

    Removed debug and made distance serializable.

--- Labyrinth-Unity/Assets/Scripts/GhostZone.cs
+++ Labyrinth-Unity/Assets/Scripts/GhostZone.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class GhostZone : MonoBehaviour
 {
     [SerializeField] private Ghost _ghostPrefab;
+    [SerializeField] private float _distanceToPlayer = 3;
 
     public void SpawnGhost(GameObject zone, Collider other)
     {
         if (other.CompareTag("Player")) 
         {
             Vector3 offset = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
-            Debug.Log(offset);
-            Vector3 ghostPosition = other.transform.position - 5 * offset;
+            Vector3 ghostPosition = other.transform.position - _distanceToPlayer * offset;
             ghostPosition.y = this.transform.position.y;
             Instantiate(_ghostPrefab, ghostPosition, Quaternion.identity, this.transform);
         }

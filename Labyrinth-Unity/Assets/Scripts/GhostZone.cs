2a1dfbebc958b023398e8ecf7eb5514bc21796c6
Anastasiya <natay1304@gmail.com>
2019-04-19 00:19:26 +0300

    Update ghost distance to player.

--- Labyrinth-Unity/Assets/Scripts/GhostZone.cs
+++ Labyrinth-Unity/Assets/Scripts/GhostZone.cs
@@ -5,16 +5,34 @@ using UnityEngine;
 public class GhostZone : MonoBehaviour
 {
     [SerializeField] private Ghost _ghostPrefab;
-    [SerializeField] private float _distanceToPlayer = 3;
+    [SerializeField] private LayerMask _wallMask;
+    [SerializeField] private float _maxDistanceToGhost;
+    
 
     public void SpawnGhost(GameObject zone, Collider other)
     {
         if (other.CompareTag("Player")) 
-        {
-            Vector3 offset = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
-            Vector3 ghostPosition = other.transform.position - _distanceToPlayer * offset;
-            ghostPosition.y = this.transform.position.y;
-            Instantiate(_ghostPrefab, ghostPosition, Quaternion.identity, this.transform);
+        {        
+            Instantiate(_ghostPrefab, GetSpawnPosition(Camera.main.transform), Quaternion.identity, this.transform);
         }
+    }
+
+    private Vector3 GetSpawnPosition(Transform playerHead)
+    {
+        RaycastHit hit;
+        Vector3 offset = Vector3.ProjectOnPlane(playerHead.forward, Vector3.up).normalized;
+
+        Vector3 spawnPosition;
+        if(Physics.Raycast(Camera.main.transform.position, offset, out hit, _maxDistanceToGhost, _wallMask))
+        {
+            spawnPosition = hit.point - offset * 0.2f;
+        }
+        else
+        {
+            spawnPosition = playerHead.position - _maxDistanceToGhost * offset;
+        }         
+        
+        spawnPosition.y = this.transform.position.y;
+        return spawnPosition;
     }
 }
